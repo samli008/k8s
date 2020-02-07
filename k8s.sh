2a920d7031b10cca7cba0d523053785784edd97e
liyang <235938019@qq.com>
2020-02-07 02:05:42 +0000

    liyang

--- k8s.sh
+++ k8s.sh
@@ -8,6 +8,7 @@ systemctl start kubelet
 
 docker load < registry2.tar
 docker run -d -v /root/registry:/var/lib/registry --name k8s_registry --restart always -p 4000:5000 registry:2
+curl http://localhost:4000/v2/_catalog
 
 cat > /etc/sysconfig/kubelet << EOF
 KUBELET_EXTRA_ARGS="--fail-swap-on=false"
