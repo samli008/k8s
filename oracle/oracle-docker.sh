3529af3cd898ac80811d9040e69d365c74df7eef
liyang <235938019@qq.com>
2020-11-20 20:52:28 -0500

    liyang

--- oracle/oracle-docker.sh
+++ oracle/oracle-docker.sh
@@ -4,6 +4,7 @@ docker run -d --name=oracle -p 8181:8080 -p 1521:1521 -v /oracle/data:/u01/app/o
 # deploy oracle11g on k8s
 kubectl apply -f oracle11g.yaml --namespace=kube-public
 kubectl get svc --namespace=kube-public
+kubectl exec oralce-68b86d5b75-vlhnp -it bash -n kube-public
 
 # k8s internal link
 hostname: oracle-svc.kube-public
@@ -24,3 +25,4 @@ password: oracle
 # oracle client with sqlplus
 sqlplus system/oracle@//192.168.8.24:1521/EE.oracle.docker
 sqlplus system/oracle@//192.168.8.24:31521/EE.oracle.docker
+sqlplus sys/oracle@//192.168.8.24:31521/EE.oracle.docker as sysdba
